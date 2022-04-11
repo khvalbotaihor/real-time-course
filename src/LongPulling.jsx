3d317454805b5800744bd90abf964eb1d5fbfa55
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-11 19:52:01 +0300

    - add onChange logic

--- src/LongPulling.jsx
+++ src/LongPulling.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const LongPulling = () => {
+    const [messages, setMessages] = useState([])
+    const [value, setValue] = useState('')
+
+    const onChangeHandler = (e) => {
+       const inputValue = e.target.value
+        setValue(inputValue)
+    }
+
     return (
         <div className="center">
         <div>
             <div className="form">
-                <input type="text"/>
+                <input type="text" value={value} onChange={onChangeHandler}/>
                 <button>Send</button>
+            </div>
+            <div className="messages">
+                {messages?.map(message =>
+                <div className="message" key={message.id}>
+                    {message.message}
+                </div>
+                )}
             </div>
         </div>
         </div>

2fec12985381907fb85e7c459051929d35ded00c
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-12 09:45:01 +0300

    - check longPulling functionality on chrome window and incogniyo window and everything works fine, messages from 1 window present in second window and vise versa

--- src/LongPulling.jsx
+++ src/LongPulling.jsx
@@ -1,9 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
 const LongPulling = () => {
     const [messages, setMessages] = useState([])
     const [value, setValue] = useState('')
+
+    useEffect(() => {
+        subscribe()
+    },[])
+
+    const subscribe = async () => {
+        try{
+          const {data} = await axios.get('http://localhost:5000/get-messages')
+          setMessages(prev => [data, ...prev])
+            await subscribe()
+        }catch (e){
+            setTimeout(() => {
+                subscribe()
+            }, 500)
+        }
+    }
 
     const onChangeHandler = (e) => {
        const inputValue = e.target.value
